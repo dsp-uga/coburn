a0d605db1de3eb629f7aa3e736a4b987c25798fa
vibodh01 <vibodh.phenani@gmail.com>
2018-03-29 12:39:08 -0400

    Updated preprocess.py file
    Added functions for gaussian smoothing ,median filter , Standard deviation

--- coburn/data/preprocess.py
+++ coburn/data/preprocess.py
@@ -21,3 +21,55 @@ class Variance(Transform):
     """
     def __call__(self, images):
         return images.var()
+
+
+class Gaussian(Transform):
+    """
+    Computes the gaussian smoothing of images along the time axis
+    """
+
+    def __call__(self, series):
+        images=series.toimages()
+        return images.gaussian_filter(sigma=2,order=0).values[0];
+
+
+class Median(Transform):
+    """
+    Computes the median filter of smoothing the images along the time axis
+    """
+
+    def __call__(self, series):
+        images = series.toimages()
+        return images.median_filter(size=2).values[0];
+
+
+class Deviation(Transform):
+
+    """
+    Computes the standard deviation of images along the time axis
+    """
+
+    def __call__(self, series):
+        images = series.toimages()
+        return images.std().values[0];
+
+
+class Subtract(Transform):
+    """
+    This will subtract given value from all the images
+
+    """
+    def __call__(self, series):
+        images = series.toimages()
+
+        return images.subtract(2).values[0];
+
+class UniformFilter(Transform):
+    """
+    Applies uniform filter to all the images
+
+    """
+
+    def __call__(self, series):
+        images = series.toimages()
+        return images.uniform_filter(2).values[0];
