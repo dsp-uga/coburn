ff7e8cab6f54bef0636ea4968f1934effd16b9c6
raj-sivakumar <raji.a.siva@gmail.com>
2018-04-04 18:56:52 -0400

    max_fram_dims.py

--- coburn/experiments/max_frame_dims.py
+++ coburn/experiments/max_frame_dims.py
@@ -12,6 +12,7 @@ def main():
     #                                'a7e37600a431fa6d6023514df87cfc8bb5ec028fb6346a10c2ececc563cc5423',
     #                                '70a6300a00dbac92be9238252ee2a75c86faf4729f3ef267688ab859eed1cc60'])
     dataset = loader.load(samples='all')
+    print("samples loaded")
     # transforms = Compose([cuda_transform])
     max_row = 0
     max_cols = 0
