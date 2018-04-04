d8f29402765c0ec3af08f8314faa198b0201642a
raj-sivakumar <raji.a.siva@gmail.com>
2018-04-04 02:58:18 -0400

    fix dataset array transformation

--- coburn/data/fft_features.py
+++ coburn/data/fft_features.py
@@ -1,11 +1,6 @@
 import numpy as np
 from . import Transform
 
-# class Frequency(Transform):
-#     def __call__(self, images):
-#         return images.map_as_series(func=self.fourier)
-#     def fourier(self, arr):
-#         n = 128
-#         sp = np.fft.fft(arr, n=n)
-#         # freq = np.fft.fftfreq(n=n, d=.02)
-#         return sp
+class Frequency(Transform):
+    def __call__(self, images):
+        return images.toarray()
--- coburn/experiments/fft_hist_test.py
+++ coburn/experiments/fft_hist_test.py
@@ -15,9 +15,10 @@ def main():
     dataset = loader.load(samples=['4bad52d5ef5f68e87523ba40aa870494a63c318da7ec7609e486e62f7f7a25e8',
                                    'a7e37600a431fa6d6023514df87cfc8bb5ec028fb6346a10c2ececc563cc5423',
                                    '70a6300a00dbac92be9238252ee2a75c86faf4729f3ef267688ab859eed1cc60'])
+    fft_transform = fft_features.Frequency()
+    dataset.set_transform(fft_transform)
     for i in range(len(dataset)):
-        sample = dataset[i] #thunder image series
-        dataset[i] = torch.from_numpy(sample.toarray()).cuda() #turn into cuda tensor
+         #thunder image series
         print(dataset[i].type())
 
         print(dataset[i].shape)
