3247bb33c93ac64f7f40fead8bd4505fe499c388
raj-sivakumar <raji.a.siva@gmail.com>
2018-04-04 02:53:02 -0400

    fix for loop indent

--- coburn/experiments/fft_hist_test.py
+++ coburn/experiments/fft_hist_test.py
@@ -15,8 +15,7 @@ def main():
     dataset = loader.load(samples=['4bad52d5ef5f68e87523ba40aa870494a63c318da7ec7609e486e62f7f7a25e8',
                                    'a7e37600a431fa6d6023514df87cfc8bb5ec028fb6346a10c2ececc563cc5423',
                                    '70a6300a00dbac92be9238252ee2a75c86faf4729f3ef267688ab859eed1cc60'])
-for i in range(len(dataset)):
-
+    for i in range(len(dataset)):
         sample = dataset[i] #thunder image series
         dataset[i] = torch.from_numpy(sample.toarray()).cuda() #turn into cuda tensor
         print(dataset[i].type())
