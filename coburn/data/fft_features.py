e37dc5f4235d6536eafd28d94ea03d2561dd3393
raj-sivakumar <raji.a.siva@gmail.com>
2018-04-04 03:19:22 -0400

    rename transform function

--- coburn/data/fft_features.py
+++ coburn/data/fft_features.py
@@ -2,6 +2,6 @@ import numpy as np
 from .Transform import Transform
 import torch
 
-class Frequency(Transform):
+class MakeCUDA(Transform):
     def __call__(self, images):
         return torch.from_numpy(images.toarray()).float().cuda()
