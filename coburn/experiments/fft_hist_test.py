a8dcd350ea3da8af7cb28383e8b22ef11253469d
raj-sivakumar <raji.a.siva@gmail.com>
2018-04-04 03:21:07 -0400

    update transforms in test file

--- coburn/experiments/fft_hist_test.py
+++ coburn/experiments/fft_hist_test.py
@@ -15,8 +15,9 @@ def main():
     dataset = loader.load(samples=['4bad52d5ef5f68e87523ba40aa870494a63c318da7ec7609e486e62f7f7a25e8',
                                    'a7e37600a431fa6d6023514df87cfc8bb5ec028fb6346a10c2ececc563cc5423',
                                    '70a6300a00dbac92be9238252ee2a75c86faf4729f3ef267688ab859eed1cc60'])
-    fft_transform = fft_features.Frequency()
-    dataset.set_transform(fft_transform)
+    cuda_transform = fft_features.MakeCUDA()
+    transforms = Compose([cuda_transform])
+    dataset.set_transform(transforms)
     for i in range(len(dataset)):
          #thunder image series
         print(dataset[i].type())
