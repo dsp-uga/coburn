3f189f45ff57d9f78190ae5cfab3a794a9a15ae9
raj-sivakumar <raji.a.siva@gmail.com>
2018-04-04 02:03:57 -0400

    going to ditch the Transform and use PyTorch to perfom full FFT pipeline

--- coburn/experiments/fft_hist_test.py
+++ coburn/experiments/fft_hist_test.py
@@ -16,22 +16,6 @@ def main():
                                    'a7e37600a431fa6d6023514df87cfc8bb5ec028fb6346a10c2ececc563cc5423',
                                    '70a6300a00dbac92be9238252ee2a75c86faf4729f3ef267688ab859eed1cc60'])
 
-    # compose the Mean and Variance transforms (this composition is rather meaningless, but is a good example of how this works)
-    # note that order matters!  The transforms will be applied in order
-    fft_transform = fft_features.Frequency()
-
-    # hist_transform = fft_features.Histogram()
-    composed_transform = Compose([fft_transform])
-    composed_transform
-    dataset.set_transform(composed_transform)
-
-    fft_images = list()
-
-    torch.Size(dataset)
     for i in range(len(dataset)):
-        sample = dataset[i,:,:]  # mean transform has already been applied!
-        print(sample)
-        fft_images.append(sample)
-
-    tile(fft_images)
-    plt.show()
+        dataset[i].toarray()
+        dataset[i].shape
