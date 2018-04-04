924167aa30f122da530720dc08a6b921dea59709
raj-sivakumar <raji.a.siva@gmail.com>
2018-04-04 03:14:04 -0400

    hopefully turn np array to Torch cuda float tensor(this time)

--- coburn/data/fft_features.py
+++ coburn/data/fft_features.py
@@ -4,4 +4,5 @@ import torch
 
 class Frequency(Transform):
     def __call__(self, images):
+        torch.set_default_tensor_type("torch.FloatTensor")
         return torch.from_numpy(images.toarray()).cuda()
