a1af423144a2a4d4fcdee938ca28d27962cdfe2c
raj-sivakumar <raji.a.siva@gmail.com>
2018-04-05 04:43:57 -0400

    fix error

--- coburn/experiments/fft_hist_test.py
+++ coburn/experiments/fft_hist_test.py
@@ -15,7 +15,7 @@ def main():
                                    'a7e37600a431fa6d6023514df87cfc8bb5ec028fb6346a10c2ececc563cc5423',
                                    '70a6300a00dbac92be9238252ee2a75c86faf4729f3ef267688ab859eed1cc60'])
     resize_transform = preprocess.Resize(dataset, 640, 480)
-    fft_transform = fft_features.Frequency(n=128))
+    fft_transform = fft_features.Frequency(n=128)
     cuda_transform = tvt.Lambda(lambda x: torch.from_numpy(x).float().cuda())
     submean_tranform = tvt.Lambda(lambda x: x.sub(torch.mean(x, dim=0)))
 
