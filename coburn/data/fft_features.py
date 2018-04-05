ae189f40b8b0d3c093b71c2db90a15ff2ef425f0
raj-sivakumar <raji.a.siva@gmail.com>
2018-04-05 03:05:06 -0400

    try new transform approach

--- coburn/data/fft_features.py
+++ coburn/data/fft_features.py
@@ -3,6 +3,7 @@ from .Transform import Transform
 import torch
 import torchvision.transforms as tvt
 
+
 class MakeCUDA(Transform):
     def __call__(self, images):
         return torch.from_numpy(images.toarray()).float().cuda()
--- coburn/experiments/fft_hist_test.py
+++ coburn/experiments/fft_hist_test.py
@@ -1,13 +1,13 @@
 """
 This experiment shows an example of how the loader and preprocess modules can be used to load a dataset and
-do some simple preprocessing
+do some simple preprocessing (NEEDS cuda)
 """
 from coburn.data import preprocess, loader, fft_features
 from torchvision.transforms import Compose
 import torchvision.transforms as tvt
 import torch
 import numpy as np
-
+import pytorch_fft.fft as fft
 
 
 def main():
@@ -15,12 +15,14 @@ def main():
     dataset = loader.load(samples=['4bad52d5ef5f68e87523ba40aa870494a63c318da7ec7609e486e62f7f7a25e8',
                                    'a7e37600a431fa6d6023514df87cfc8bb5ec028fb6346a10c2ececc563cc5423',
                                    '70a6300a00dbac92be9238252ee2a75c86faf4729f3ef267688ab859eed1cc60'])
+    resize_transform = preprocess.Resize(dataset, 640, 480)
     cuda_transform = fft_features.MakeCUDA()
-    fft_transform = tvt.Lambda(lambda x: np.fft.fft(x, n=128, axis=0))
+    fft_transform = tvt.Lambda(lambda x: fft.fft2(x, n=128, axis=0)[0])
+    submean_tranform = tvt.Lambda(lambda x: x.sub(torch.mean(x, dim=0)))
 
-    transforms = Compose([cuda_transform, fft_transform])
+    transforms = Compose([resize_transform, cuda_transform, fft_transform, submean_tranform])
     dataset.set_transform(transforms)
+    mean_dataset = dataset.set_transform(mean_transform)
     for i in range(len(dataset)):
-         #thunder image series
         print(dataset[i].type())
         print(dataset[i].shape)
